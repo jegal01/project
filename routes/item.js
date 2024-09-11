e39fcdf7e5f9ef2d3724505483e097e28990ad68
NamcheolHer <nc.her@modulabs.co.kr>
2024-09-11 11:50:35 +0900

    백엔드 코드 수정 완료

--- routes/item.js
+++ routes/item.js
@@ -12,12 +12,13 @@ const prisma = new PrismaClient({
 // [필수] 1. 아이템 생성
 // 1. 아이템 코드, 아이템 명, 아이템 능력, 아이템 가격을 req(request)에서 json으로 전달받기
 // 2. 데이터베이스에 아이템 저장하기
-router.post('/item/create', async (req, res) => {
+router.post('/item/create/:itemId', async (req, res) => {
   try {
-    const itemCode = req.closed.item_code;
+    const itemCode = req.body.item_code;
     const itemName = req.body.item_name;
     const atk = req.body.atk;
     const price = req.body.price;
+
     const createItem = await prisma.item.create({
       data: {
         itemCode: itemCode,
@@ -27,6 +28,7 @@ router.post('/item/create', async (req, res) => {
       },
     });
 
+    res.status(200).json({ item_info: createItem });
     console.log(createItem);
   } catch (error) {
     res.status(500).json({ error: `아이템 입력에 실패했습니다.` });
